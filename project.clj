6d7a6fab1cbdcad50be610393b41616b998b1e87
Hamish Hutchings <hamhut1066@gmail.com>
2016-02-19 16:03:25 +0000

    move to async library http-kit

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.7.0"],
-                 [clj-http "2.0.1"],
-                 [cheshire "5.5.0"],
-                 [org.clojure/core.match "0.3.0-alpha4"],
+                 [org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.1"]
+                 [http-kit "2.1.19"]
+                 [cheshire "5.5.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/data.json "0.2.6"]])
