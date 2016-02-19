73551775346b624e1452b36832cec78f55c8cd46
Hamish Hutchings <hamhut1066@gmail.com>
2016-02-19 16:05:37 +0000

    Change to use Async Library

--- src/ship.clj
+++ src/ship.clj
@@ -1,6 +1,6 @@
 (ns ship
   (:require
-   [clj-http.client :as client]
+   [org.httpkit.client :as client]
    [clojure.data.json :as json]
    ))
 (require '[clojure.core.match :refer [match]])
@@ -11,7 +11,9 @@
 
 ;; helpers
 (defn body [request]
-  (json/read-json (get request :body)))
+  (try
+    (json/read-json (get request :body))
+    (catch Exception e (merge request {:error true}))))
 
 (defn- identifier [object]
   (match [object]
@@ -31,34 +33,40 @@
   ([token form params]
    (let [core {
                :insecure? true
-               :content-type :json
+               :headers (merge (post_headers token) {"Content-Type" "application/json"})
                }
          payload (match [form]
                         [:get] {
-                                :headers (post_headers token)
                                 :query-params params
+                                :content-type :json
                                 }
                         [:put] {
-                                :headers (post_headers token)
-                                :form-params params}
-                        [:delete] {
-                                   :headers (post_headers token)}
+                                :accept :json
+                                :body (json/write-str params)}
+                        [:delete] {}
                         :else {
                                :body (if (or (string? params) (nil? params)) params (json/write-str params))
                                :headers (post_headers token)})]
      (merge core payload))))
 (defn- debug [heads] (merge heads {:debug true :save-request? true}))
 
+(defmacro sync-call [proto url meta]
+  `(let [{:keys [status# headers# body# error#] :as resp#}
+         @(~proto ~url ~meta)]
+     (if error#
+       nil
+       (body resp#))))
+
 (defmacro def-resource [func_name url]
   "Define the endpoints for a specific resource on the backend"
   `(defn ~func_name
-     ([token#] (body (client/get (construct_url ~url) (meta_data token#))))
+     ([token#] (sync-call client/get ~(construct_url url) (meta_data token#)))
      ([token# pred#]
-      (body (client/get (construct_url ~(str url "/search")) (meta_data token# :get pred#))))
+      (sync-call client/get ~(construct_url (str url "/search")) (meta_data token# :get pred#)))
      ([token# method# pred# & [id#]]
       (match [method#]
-             [:new] (body (client/post (construct_url ~url) (meta_data token# :post pred#)))
-             [:update] (body (client/patch (construct_url (str ~url "/" id#)) (meta_data token# :post pred#)))
+             [:new] (sync-call client/post ~(construct_url url) (meta_data token# :post pred#))
+             [:update] (sync-call client/patch (construct_url (str ~url "/" id#)) (meta_data token# :post pred#))
              :else "Unsupported value"))))
 
 (def-resource users "users")
@@ -72,15 +80,16 @@
 ;; keyword definitions
 (defn problem_keyword_set [token identifier keyword & [value]]
   "Update and existing problem and add/update a keyword"
-  (client/put (construct_url (str "problems/" identifier "/keywords/" keyword)) (meta_data token :put (if (nil? value)
-                                                                                                        ""
-                                                                                                        value))))
+  (sync-call client/put (construct_url (str "problems/" identifier "/keywords/" keyword)) (meta_data token :put (if (nil? value)
+                                                                                      ""
+                                                                                      value))))
 
 (defn problem_keyword_delete [token identifier keyword]
   "delete keyword from problem"
   (client/delete (construct_url (str "problems/" identifier "/keywords/" keyword)) (meta_data token :delete nil)))
-(defn me [token]
+(defn me
   "Get Current User"
+  [token]
   (get (users token {:predicate "identifier = $ApiUser"}) 0))
 
 (defn users_active [token]
